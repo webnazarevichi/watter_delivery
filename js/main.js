db20098e9caa3d05ab4cbc0c36d93a59f2ecd11a
Andrey <nazarevich.andrey@mail.ru>
2021-08-06 21:41:30 +0300

    viewport575px

--- js/main.js
+++ js/main.js
@@ -10,7 +10,16 @@ $(document).ready(function(){
     $('.sale__carousel').slick({
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-      speed: 1200
+      speed: 1200,
+	  responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				dots: true,
+				arrows: false
+			}
+		}
+	  ]
     //   autoplay: true,
     //   autoplaySpeed: 3000
     });
